aae2daba2396381085a37c13684fe73d40cf344c
wangjie <wangjie_fourth@163.com>
2022-08-24 22:30:13 +0800

    temp

---
+++ src/main/java/SortArrayByParity.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+
+
+public class SortArrayByParity {
+
+    public int[] sortArrayByParity(int[] nums) {
+        if (nums == null || nums.length < 1) {
+            return nums;
+        }
+
+        int start = 0;
+        int end = nums.length - 1;
+        while (start < end) {
+            while (start < end && nums[start] % 2 == 0){
+                start++;
+            }
+            while (start < end && nums[end] % 2 == 1) {
+                end--;
+            }
+
+            if (start < end) {
+                swap(nums, start, end);
+                start++;
+                end--;
+            }
+        }
+
+        return nums;
+    }
+
+    private static void swap(int[] nums, int a, int b) {
+        if (nums == null || a >= nums.length || b >= nums.length) {
+            return;
+        }
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+
+    public static void main(String[] args) {
+        SortArrayByParity sortArrayByParity = new SortArrayByParity();
+        int[] input = {0,1,2};
+        int[] output = {0,2, 1};
+
+        System.out.println(Arrays.equals(sortArrayByParity.sortArrayByParity(input), output));
+
+    }
+
+
+}
---
+++ src/main/java/ThreeSum.java
@@ -0,0 +1,74 @@
+import java.util.Arrays;
+
+public class ThreeSum {
+
+    public int[] threeSum(int[] nums, int target) {
+        int[] result = new int[3];
+        if (nums.length < 3) {
+            return result;
+        }
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[i] + nums[j] + nums[k] == target) {
+                        // 假设输出不要求顺序
+                        result[0] = nums[i];
+                        result[1] = nums[j];
+                        result[2] = nums[k];
+                        return result;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    // ---
+    public int[] threeSumV2(int[] nums, int target) {
+        int[] result = new int[3];
+        if (nums.length < 3) {
+            return result;
+        }
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            int nowTarget = target - nums[i];
+            int first = i+1;
+            int second = nums.length - 1;
+            while (first < second) {
+                if (nums[first] + nums[second] == nowTarget) {
+                    result[0] = nums[i];
+                    result[1] = nums[first];
+                    result[2] = nums[second];
+                    return result;
+                }
+                if (nums[first] + nums[second] > nowTarget) {
+                    second--;
+                }
+                if (nums[first] + nums[second] < nowTarget) {
+                    first++;
+                }
+            }
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        ThreeSum threeSum = new ThreeSum();
+
+        int[] nums = {-1, 0, 1, 2, -4};
+        int target = 0;
+        int[] result = {-1, 0, 1};
+
+        // ==============
+
+        System.out.println(Arrays.equals(threeSum.threeSum(nums, target), result));
+        System.out.println(Arrays.toString(threeSum.threeSum(nums, target)));
+
+        System.out.println(Arrays.equals(threeSum.threeSumV2(nums, target), result));
+        System.out.println(Arrays.toString(threeSum.threeSumV2(nums, target)));
+
+    }
+}
