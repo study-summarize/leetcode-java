3aa65b95291b747c8d0a950750ef04f9b142f5bd
wangjie <jwang55@trip.com>
2023-02-03 14:46:32 +0800

    add code

--- src/main/java/leetcode/Test_133_cloneGraph.java
+++ src/main/java/leetcode/Test_133_cloneGraph.java
@@ -1,9 +1,6 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author jwang55
@@ -11,6 +8,41 @@ import java.util.Map;
 public class Test_133_cloneGraph {
 
     public Node cloneGraph(Node node) {
+        if (node == null) {
+            return null;
+        }
+        // key是原图节点，value是新图节点
+        Map<Node, Node> map = new HashMap<>();
+
+        return bfs(node, map);
+    }
+    private Node bfs(Node node, Map<Node, Node> map) {
+        // 已经复制过的节点直接返回
+        if (map.containsKey(node)) {
+            return map.get(node);
+        }
+
+        // 1、创建队列
+        Queue<Node> queue = new LinkedList<>();
+        // 2、将初始节点放入队列
+        queue.add(node);
+        // 3、bfs遍历
+        Node copyNode = new Node(node.val);
+        map.put(node, copyNode);
+        while (!queue.isEmpty()) {
+            Node curNode = queue.poll();
+            for (Node adjNode : curNode.neighbors) {
+                Node adjCopyNode = bfs(adjNode, map);
+                copyNode.neighbors.add(adjCopyNode);
+            }
+        }
+        return copyNode;
+    }
+
+    /**
+     * 使用DFS方法
+     */
+    public Node cloneGraphWithDfs(Node node) {
         if (node == null) {
             return null;
         }
--- src/main/java/leetcode/Test_261_validTree.java
+++ src/main/java/leetcode/Test_261_validTree.java
@@ -1,9 +1,6 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author jwang55
@@ -11,6 +8,56 @@ import java.util.Map;
 public class Test_261_validTree {
 
     public boolean validTree(int n, int[][] edges) {
+        if (n == 0 || edges == null || edges.length == 0
+                || edges[0] == null || edges[0].length == 0) {
+            return false;
+        }
+        // 满足是树的条件
+        // （1）边数等于节点数 - 1
+        if (edges.length != n - 1) {
+            return false;
+        }
+
+        // （2）图的连通分量为1
+        int count = 0;
+        // 从每个节点开始遍历
+        boolean[] visited = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            // 防止重复访问
+            if (!visited[i]) {
+                bfs(edges, visited, i);
+                count++;
+            }
+        }
+        return count == 1;
+    }
+
+    private void bfs(int[][] edges, boolean[] visited, int node) {
+        // 1、构建队列
+        Queue<Integer> queue = new LinkedList<>();
+        // 2、将初始节点加入队列，并标记
+        queue.offer(node);
+        visited[node] = true;
+        // 3、bfs遍历
+        while (!queue.isEmpty()) {
+            Integer curNode = queue.poll();
+            // 在邻接矩阵中找到当前节点的邻接节点
+            int length = edges[curNode].length;
+            for (int i = 0; i < length; i++) {
+                // 只有值为1时，才是节点；不能重复访问
+                if (edges[curNode][i] == 1 && !visited[i]) {
+                    // 加入队列，并标记
+                    queue.offer(i);
+                    visited[i] = true;
+                }
+            }
+        }
+    }
+
+    /**
+     * 通过DFS方式计算
+     */
+    public boolean validTreeWithDfs(int n, int[][] edges) {
         if (n == 0) {
             return false;
         }
