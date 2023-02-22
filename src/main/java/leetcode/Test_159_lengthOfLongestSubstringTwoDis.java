6e5082ab3fdb828728f51d1c4839caa283e75968
wangjie <jwang55@trip.com>
2023-02-22 19:54:42 +0800

    add code

---
+++ src/main/java/leetcode/Test_159_lengthOfLongestSubstringTwoDis.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+/**
+ * @author jwang55
+ */
+public class Test_159_lengthOfLongestSubstringTwoDis {
+
+    public int lengthOfLongestSubstringTwoDis(String s) {
+        return 0;
+
+    }
+}
