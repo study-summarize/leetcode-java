368c8ac7b1d7a5154d19441783eb41ad33622bea
wangjie <jwang55@trip.com>
2023-02-28 10:04:12 +0800

    add code

--- src/main/java/leetcode/Test_028_strStr.java
+++ src/main/java/leetcode/Test_028_strStr.java
@@ -2,7 +2,37 @@ package leetcode;
 
 public class Test_028_strStr {
 
-    public int strStr(String haystack, String needle) {
-        return 0;
+    /**
+     * 方法一：暴力解法
+     */
+    public int strStr01(String haystack, String needle) {
+        if (haystack == null || haystack.length() == 0 || needle == null || needle.length() == 0) {
+            return -1;
+        }
+
+        int sLen = haystack.length();
+        int tLen = needle.length();
+
+        for (int i = 0; i <= sLen - tLen; i++) {
+            int j = 0;
+            while (j < tLen) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    break;
+                }
+                j++;
+            }
+            if (j == tLen) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * 方法二：Robin-Karp算法
+     */
+    public int strStr02(String haystack, String needle) {
+        return -1;
     }
 }
--- src/main/java/leetcode/Test_209_minSubArrayLen.java
+++ src/main/java/leetcode/Test_209_minSubArrayLen.java
@@ -3,6 +3,30 @@ package leetcode;
 public class Test_209_minSubArrayLen {
 
     public int minSubArrayLen(int target, int[] nums) {
-        return 0;
+        if (nums == null || nums.length == 0 || target <= 0) {
+            return 0;
+        }
+        int result = Integer.MAX_VALUE;
+        int sum = 0;
+        int i = 0;
+        int j = 0;
+
+        for (i = 0; i < nums.length; i++) {
+            while (j < nums.length) {
+                if (sum < target) {
+                    sum += nums[j];
+                    j++;
+                } else {
+                    break;
+                }
+            }
+            // 窗口收缩
+            if (sum >= target) {
+                result = Math.min(result, j - i);
+            }
+            sum -= nums[i];
+        }
+
+        return result == Integer.MAX_VALUE ? 0 : result;
     }
 }
