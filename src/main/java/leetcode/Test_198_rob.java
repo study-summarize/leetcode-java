741dc9a66d05a3be29caf662ab13a05a06bb8dc7
wangjie <jwang55@trip.com>
2023-03-18 16:06:21 +0800

    add code

--- src/main/java/leetcode/Test_198_rob.java
+++ src/main/java/leetcode/Test_198_rob.java
@@ -1,7 +1,29 @@
 package leetcode;
 
 public class Test_198_rob {
+
+    /**
+     * 滚动数组优化、动态规划
+     */
     public int rob(int[] nums) {
-        return 0;
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        // dp[i]：在第i个房子时，获得的最大金额
+        int[] dp = new int[2];
+        dp[0] = 0;
+        dp[1] = nums[0];
+
+        // function: dp[i]
+        for (int i = 2; i <= nums.length; i++) {
+            dp[i % 2] = Math.max(
+                    dp[(i - 1) % 2],
+                    dp[(i - 2) % 2] + nums[i - 1] // nums[i-1]表示第n-2个房子
+            );
+        }
+
+        // solution
+        return dp[nums.length % 2];
     }
 }
--- src/main/java/leetcode/Test_300_lengthOfLIS.java
+++ src/main/java/leetcode/Test_300_lengthOfLIS.java
@@ -1,7 +1,39 @@
 package leetcode;
 
+import java.util.Arrays;
+
 public class Test_300_lengthOfLIS {
+
+    /**
+     * 动态规划、制表法找function
+     */
     public int lengthOfLIS(int[] nums) {
-        return 0;
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        // state：dp[i] ：以nums[i]结尾的最长严格递增子序列的长度
+        // 由于最终要遍历dp数组取最长严格递增子序列的长度，所以无法状态压缩，不能用滚动数组
+        int[] dp = new int[nums.length];
+
+        // condition：每个以nums[i]结尾的最长严格递增子序列的最小长度至少为1
+        Arrays.fill(dp, 1);
+
+        // function：制表法得到递归函数表达式
+        for (int i = 0; i < nums.length; i++) {
+            // 遍历找之前结尾比nums[i]小的递增子序列，然后找到其中最长的
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        // solution：遍历dp数组计算结果
+        int result = 0;
+        for (int value : dp) {
+            result = Math.max(result, value);
+        }
+        return result;
     }
 }
