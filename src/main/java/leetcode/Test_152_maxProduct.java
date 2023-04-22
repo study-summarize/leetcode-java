01269880a7d254e8c8a8063371ca8a63e643dbe4
wangjie <jwang55@trip.com>
2023-04-22 10:44:39 +0800

    add code

--- src/main/java/leetcode/Test_152_maxProduct.java
+++ src/main/java/leetcode/Test_152_maxProduct.java
@@ -1,7 +1,14 @@
 package leetcode;
 
 public class Test_152_maxProduct {
+    /**
+     * 动态规划
+     */
     public int maxProduct(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
         return 0;
     }
 }
