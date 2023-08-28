7cc7a59c7ebb0578c35054575e7aed9d283741e1
wangjie <wangjie_fourth@163.com>
2023-08-28 22:51:09 +0800

    俩数之和

--- src/main/java/leetcode/Test_001_twoSum.java
+++ src/main/java/leetcode/Test_001_twoSum.java
@@ -1,16 +1,68 @@
 package leetcode;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- *
- *
  * @author jwang55
  */
 public class Test_001_twoSum {
 
-    public int[] twoSum(int[] nums, int target) {
+    /**
+     * 暴力解法1：双层for循环
+     */
+    public int[] twoSum01(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return new int[2];
+        }
+
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 排序+双指针，求具体值，而不是下标
+     * 时间复杂度：O(nlogn)
+     */
+    public int[] twoSum02(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+
+        // 排序：O(nlogn)
+        Arrays.sort(nums);
+        int start = 0;
+        int end = nums.length - 1;
+
+        // 二分法一定要这样写，记得模板
+        while (start < end) {
+            if (nums[start] + nums[end] == target) {
+                return new int[]{nums[start], nums[end]};
+            }
+            if (nums[start] + nums[end] > target) {
+                end--;
+            }
+            if (nums[start] + nums[end] < target) {
+                start++;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 哈希表
+     */
+    public int[] twoSum03(int[] nums, int target) {
         if (nums == null || nums.length == 0) {
             return new int[2];
         }
--- src/test/java/leetcode/Test_001_twoSumTest.java
+++ src/test/java/leetcode/Test_001_twoSumTest.java
@@ -15,11 +15,12 @@ public class Test_001_twoSumTest {
 
     private final Test_001_twoSum test001TwoSum = new Test_001_twoSum();
 
+
     @DisplayName("成功测试用例")
     @ParameterizedTest
     @MethodSource("generateRightCase")
-    public void test(int[] nums, int target, int[] expectedResults) {
-        int[] actResult = test001TwoSum.twoSum(nums, target);
+    public void test01(int[] nums, int target, int[] expectedResults) {
+        int[] actResult = test001TwoSum.twoSum01(nums, target);
         Assertions.assertEquals(expectedResults.length, actResult.length);
         Assertions.assertEquals(2, actResult.length);
         if (expectedResults[0] == actResult[0]) {
@@ -30,10 +31,50 @@ public class Test_001_twoSumTest {
         }
     }
 
+    @DisplayName("成功测试用例")
+    @ParameterizedTest
+    @MethodSource("generateRight02Case")
+    public void two02(int[] nums, int target, int[] expectedResults) {
+        int[] actResult = test001TwoSum.twoSum02(nums, target);
+        Assertions.assertEquals(expectedResults.length, actResult.length);
+        Assertions.assertEquals(2, actResult.length);
+        if (expectedResults[0] == actResult[0]) {
+            Assertions.assertEquals(expectedResults[1], actResult[1]);
+        } else {
+            Assertions.assertEquals(expectedResults[0], actResult[1]);
+            Assertions.assertEquals(expectedResults[1], actResult[0]);
+        }
+    }
+
+    @DisplayName("成功测试用例")
+    @ParameterizedTest
+    @MethodSource("generateRightCase")
+    public void test03(int[] nums, int target, int[] expectedResults) {
+        int[] actResult = test001TwoSum.twoSum03(nums, target);
+        Assertions.assertEquals(expectedResults.length, actResult.length);
+        Assertions.assertEquals(2, actResult.length);
+        if (expectedResults[0] == actResult[0]) {
+            Assertions.assertEquals(expectedResults[1], actResult[1]);
+        } else {
+            Assertions.assertEquals(expectedResults[0], actResult[1]);
+            Assertions.assertEquals(expectedResults[1], actResult[0]);
+        }
+    }
+
+
     public static Stream<Arguments> generateRightCase() {
         return Stream.of(
-                arguments(new int[]{2, 7, 11, 15}, 9, new int[]{0, 1})
+                arguments(new int[]{2, 7, 11, 15}, 9, new int[]{0, 1}),
+                arguments(new int[]{3, 3}, 6, new int[]{0, 1}),
+                arguments(new int[]{3, 2, 4}, 6, new int[]{1, 2})
+        );
+    }
 
+    public static Stream<Arguments> generateRight02Case() {
+        return Stream.of(
+                arguments(new int[]{2, 7, 11, 15}, 9, new int[]{2, 7}),
+                arguments(new int[]{3, 3}, 6, new int[]{3, 3}),
+                arguments(new int[]{3, 2, 4}, 6, new int[]{2, 4})
         );
     }
 }
