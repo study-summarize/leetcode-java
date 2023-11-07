215d6ebff23bf5d776474aead7e5fbdcd2c30cc6
wangjie <jwang55@trip.com>
2023-11-07 18:36:12 +0800

    add: 三数之和

--- src/main/java/leetcode/Test_015_threeSum.java
+++ src/main/java/leetcode/Test_015_threeSum.java
@@ -5,34 +5,62 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
+ * Origin:
+ *
+ *
+ * Change:
+ * 给定一个包含n个整数的数组（无重复元素）nums和一个目标值target，找出数组中和为目标值的三个数。可以假设每个输入只对应一种答案。
+ *
  * @author jwang55
  */
 public class Test_015_threeSum {
 
     /**
-     * 暴力解法：三层for循环
+     * 消元：转换成俩数之和
+     * 时间复杂度：O(nlogn) + nO(n) = O(n^2);
+     * 空间负责度：O（1）
      */
-    public int[] threeSum01(int[] nums, int target) {
+    public List<List<Integer>> threeSum01(int[] nums) {
         if (nums == null || nums.length == 0) {
             return null;
         }
+        Arrays.sort(nums);
 
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                for (int m = j + 1; m < nums.length; m++) {
-                    if (nums[i] + nums[j] + nums[m] == target) {
-                        return new int[]{nums[i], nums[j], nums[m]};
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            int start = i + 1;
+            int end = nums.length - 1;
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            while (start < end) {
+                int sum = nums[i] + nums[start] + nums[end];
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[start], nums[end]));
+                    start++;
+                    end--;
+                    if (start > 1 && nums[start] == nums[start - 1]) {
+                        start++;
+                        continue;
                     }
+                    if (end < nums.length - 1 && nums[end] == nums[end + 1]) {
+                        end--;
+                        continue;
+                    }
+                } else if (sum > 0) {
+                    end--;
+                } else {
+                    start++;
                 }
             }
         }
-        return null;
+        return result;
     }
 
     /**
-     * 方法二：将题目看成俩数之和
+     * 模板代码
      */
-    public List<List<Integer>> threeSum02(int[] nums) {
+    public List<List<Integer>> threeSumExample(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         if (nums == null || nums.length == 0) {
             return result;
@@ -76,5 +104,57 @@ public class Test_015_threeSum {
             }
         }
         return result;
+    }
+
+    /**
+     * 暴力解法：三层for循环
+     * 时间复杂度：O(n^3)
+     * 空间复杂度：O(1)
+     */
+    public int[] threeSumChange01(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int m = j + 1; m < nums.length; m++) {
+                    if (nums[i] + nums[j] + nums[m] == target) {
+                        return new int[]{nums[i], nums[j], nums[m]};
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 消元：转换成俩数之和
+     * 时间复杂度：O(nlogn) + nO(n) = O(n^2)
+     * 空间复杂度：O(1)
+     */
+    public int[] threeSumChange02(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+
+        // note: 数组无序，且返回值跟下标无关时；基本上都是需要排序
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int start = i + 1;
+            int end = nums.length - 1;
+            while (start < end) {
+                int sum = nums[i] + nums[start] + nums[end];
+                if (sum == target) {
+                    return new int[]{nums[i], nums[start], nums[end]};
+                } else if (sum < target) {
+                    start++;
+                } else {
+                    end--;
+                }
+            }
+        }
+        return null;
     }
 }
--- src/test/java/leetcode/Test_015_threeSumTest.java
+++ src/test/java/leetcode/Test_015_threeSumTest.java
@@ -17,42 +17,53 @@ public class Test_015_threeSumTest {
 
     private static final Test_015_threeSum test015ThreeSum = new Test_015_threeSum();
 
-    @DisplayName("暴力解法")
+    @DisplayName("原版题目解法")
     @ParameterizedTest
-    @MethodSource("generateRight01Case")
-    public void test01(int[] nums, int target, int[] expectedResults) {
-        int[] actResult = test015ThreeSum.threeSum01(nums, target);
-        Arrays.sort(expectedResults);
-        Arrays.sort(actResult);
-        Assertions.assertEquals(expectedResults.length, actResult.length);
-        for (int i = 0; i < expectedResults.length; i++) {
-            Assertions.assertEquals(expectedResults[i], actResult[i]);
-        }
-    }
-
-    @DisplayName("暴力解法")
-    @ParameterizedTest
-    @MethodSource("generateRight02Case")
-    public void test02(int[] nums, List<Integer> expectedResults) {
-        List<List<Integer>> actResultList = test015ThreeSum.threeSum02(nums);
-        Assertions.assertEquals(1, actResultList.size());
+    @MethodSource("generateOriginRightCase")
+    public void testOrigin(int[] nums, List<Integer> expectedResults) {
         expectedResults.sort(Integer::compareTo);
-        actResultList.get(0).sort(Integer::compareTo);
+
+        List<List<Integer>> actResultList = test015ThreeSum.threeSumExample(Arrays.copyOf(nums, nums.length));
+        List<List<Integer>> actResultList01 = test015ThreeSum.threeSum01(Arrays.copyOf(nums, nums.length));
+        Assertions.assertEquals(1, actResultList.size());
+        Assertions.assertEquals(1, actResultList01.size());
+        actResultList.forEach(x -> x.sort(Integer::compareTo));
+        actResultList01.forEach(x -> x.sort(Integer::compareTo));
 
         Assertions.assertEquals(expectedResults.size(), actResultList.get(0).size());
+        Assertions.assertEquals(expectedResults.size(), actResultList01.get(0).size());
         for (int i = 0; i < expectedResults.size(); i++) {
             Assertions.assertEquals(expectedResults.get(i), actResultList.get(0).get(i));
+            Assertions.assertEquals(expectedResults.get(i), actResultList01.get(0).get(i));
         }
     }
 
-    public static Stream<Arguments> generateRight02Case() {
+
+    @DisplayName("变版题目解法")
+    @ParameterizedTest
+    @MethodSource("generateChangeRightCase")
+    public void testChange(int[] nums, int target, int[] expectedResults) {
+        int[] actResult1 = test015ThreeSum.threeSumChange01(nums, target);
+        int[] actResult2 = test015ThreeSum.threeSumChange02(nums, target);
+        Arrays.sort(expectedResults);
+        Arrays.sort(actResult1);
+        Arrays.sort(actResult2);
+        Assertions.assertEquals(expectedResults.length, actResult1.length);
+        for (int i = 0; i < expectedResults.length; i++) {
+            Assertions.assertEquals(expectedResults[i], actResult1[i]);
+            Assertions.assertEquals(expectedResults[i], actResult2[i]);
+        }
+    }
+
+    public static Stream<Arguments> generateOriginRightCase() {
         return Stream.of(
                 arguments(new int[]{-1, 0, 1, 2, -4}, Arrays.asList(-1, 0, 1)),
+                arguments(new int[]{0, 0, 0, 0, 0}, Arrays.asList(0, 0, 0)),
                 arguments(new int[]{3, 2, -5}, Arrays.asList(3, 2, -5))
         );
     }
 
-    public static Stream<Arguments> generateRight01Case() {
+    public static Stream<Arguments> generateChangeRightCase() {
         return Stream.of(
                 arguments(new int[]{-1, 0, 1, 2, -4}, 0, new int[]{-1, 0, 1}),
                 arguments(new int[]{2, 7, 11, 15}, 20, new int[]{2, 7, 11}),
