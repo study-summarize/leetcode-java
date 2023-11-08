16cae6f55b4ec814f4228ad0910c44733621a0b8
wangjie <wangjie_fourth@163.com>
2023-11-08 21:39:19 +0800

    add: 三数之和

--- src/test/java/leetcode/LeetCode_015_threeSumTest.java
+++ src/test/java/leetcode/LeetCode_015_threeSumTest.java
@@ -14,28 +14,33 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 @DisplayName("三数之和测试数据")
-public class Test_015_threeSumTest {
+public class LeetCode_015_threeSumTest {
 
     private static final LeetCode_015_threeSum test015ThreeSum = new LeetCode_015_threeSum();
 
     @DisplayName("原版题目解法")
     @ParameterizedTest
     @MethodSource("generateOriginRightCase")
-    public void testOrigin(int[] nums, List<Integer> expectedResults) {
-        expectedResults.sort(Integer::compareTo);
+    public void testOrigin(int[] nums, List<List<Integer>> expectedResults) {
+        expectedResults.forEach(x -> x.sort(Integer::compareTo));
 
         List<List<Integer>> actResultList = test015ThreeSum.threeSumExample(Arrays.copyOf(nums, nums.length));
         List<List<Integer>> actResultList01 = test015ThreeSum.threeSum01(Arrays.copyOf(nums, nums.length));
-        Assertions.assertEquals(1, actResultList.size());
-        Assertions.assertEquals(1, actResultList01.size());
+        Assertions.assertEquals(expectedResults.size(), actResultList.size());
+        Assertions.assertEquals(expectedResults.size(), actResultList01.size());
         actResultList.forEach(x -> x.sort(Integer::compareTo));
         actResultList01.forEach(x -> x.sort(Integer::compareTo));
 
-        Assertions.assertEquals(expectedResults.size(), actResultList.get(0).size());
-        Assertions.assertEquals(expectedResults.size(), actResultList01.get(0).size());
         for (int i = 0; i < expectedResults.size(); i++) {
-            Assertions.assertEquals(expectedResults.get(i), actResultList.get(0).get(i));
-            Assertions.assertEquals(expectedResults.get(i), actResultList01.get(0).get(i));
+            Assertions.assertEquals(expectedResults.get(i).size(), actResultList.get(i).size());
+            Assertions.assertEquals(expectedResults.get(i).size(), actResultList01.get(i).size());
+        }
+
+        for (int i = 0; i < expectedResults.size(); i++) {
+            for (int j = 0; j < expectedResults.get(i).size(); j++) {
+                Assertions.assertEquals(expectedResults.get(i).get(j), actResultList.get(i).get(j));
+                Assertions.assertEquals(expectedResults.get(i).get(j), actResultList01.get(i).get(j));
+            }
         }
     }
 
@@ -58,9 +63,12 @@ public class Test_015_threeSumTest {
 
     public static Stream<Arguments> generateOriginRightCase() {
         return Stream.of(
-                arguments(new int[]{-1, 0, 1, 2, -4}, Arrays.asList(-1, 0, 1)),
-                arguments(new int[]{0, 0, 0, 0, 0}, Arrays.asList(0, 0, 0)),
-                arguments(new int[]{3, 2, -5}, Arrays.asList(3, 2, -5))
+                arguments(new int[]{-1, 0, 1, 2, -1, -4}, List.of(Arrays.asList(-1, -1, 2), Arrays.asList(-1, 0, 1))),
+                arguments(new int[]{-1, 0, 1, 2, -4}, List.of(Arrays.asList(-1, 0, 1))),
+                arguments(new int[]{0, 0, 0, 0, 0}, List.of(Arrays.asList(0, 0, 0))),
+                arguments(new int[]{0, 1, 1}, List.of()),
+                arguments(new int[]{0, 0, 0}, List.of(Arrays.asList(0, 0, 0))),
+                arguments(new int[]{3, 2, -5}, List.of(Arrays.asList(3, 2, -5)))
         );
     }
 
