abead9d1a7dece7e12d8b4c3d27b4aa413b3bfe6
wangjie <wangjie_fourth@163.com>
2023-11-08 22:27:08 +0800

    add: 区域和检索 - 数组不可变

---
+++ src/main/java/org/github/fourth/leetcode/LeetCode_303_NumArray.java
@@ -0,0 +1,15 @@
+package org.github.fourth.leetcode;
+
+public class LeetCode_303_NumArray {
+    public LeetCode_303_NumArray(int[] nums) {
+
+    }
+
+    public int sumRange(int left, int right) {
+        return 0;
+    }
+
+    public int sumRangeExample(int left, int right) {
+        return 0;
+    }
+}
