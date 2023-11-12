fa4a74ae15189966a835a263a0bb31015a66df70
wangjie <wangjie_fourth@163.com>
2023-11-12 16:39:42 +0800

    add: 下一个更大元素 I

---
+++ src/main/java/org/github/fourth/leetcode/LeetCode_496_extGreaterElement.java
@@ -0,0 +1,7 @@
+package org.github.fourth.leetcode;
+
+public class LeetCode_496_extGreaterElement {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        return nums1;
+    }
+}
