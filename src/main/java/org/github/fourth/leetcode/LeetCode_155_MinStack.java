953e61a84379efb05d5f122869b773f9c2085937
wangjie <wangjie_fourth@163.com>
2023-11-12 11:48:13 +0800

    add: 最小栈

---
+++ src/main/java/org/github/fourth/leetcode/LeetCode_155_MinStack.java
@@ -0,0 +1,33 @@
+package org.github.fourth.leetcode;
+
+public class LeetCode_155_MinStack {
+
+    public LeetCode_155_MinStack() {
+
+    }
+
+    public void push(int val) {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public int top() {
+        return 0;
+    }
+
+    public int getMin() {
+        return 0;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
