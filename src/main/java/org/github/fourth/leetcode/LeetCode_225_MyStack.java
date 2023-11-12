249ebf72e583b5d0307d2f7b40b65fe7a744a152
wangjie <wangjie_fourth@163.com>
2023-11-12 16:27:47 +0800

    add: 用队列实现栈

---
+++ src/main/java/org/github/fourth/leetcode/LeetCode_225_MyStack.java
@@ -0,0 +1,32 @@
+package org.github.fourth.leetcode;
+
+public class LeetCode_225_MyStack {
+    public LeetCode_225_MyStack() {
+
+    }
+
+    public void push(int x) {
+
+    }
+
+    public int pop() {
+        return 1;
+    }
+
+    public int top() {
+        return 1;
+    }
+
+    public boolean empty() {
+        return false;
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
