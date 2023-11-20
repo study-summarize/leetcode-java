d5d7cb14e1bd2967876ae5ddc4cc12b4f1447219
wangjie <jwang55@trip.com>
2023-11-20 14:23:41 +0800

    add: 有效的括号

--- src/main/java/org/github/fourth/leetcode/LeetCode_020_isValid.java
+++ src/main/java/org/github/fourth/leetcode/LeetCode_020_isValid.java
@@ -1,7 +1,48 @@
 package org.github.fourth.leetcode;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 给定一个只包括`'('`，`')'`，`'{'`，`'}'`，`'['`，`']'`的字符串`s`，判断字符串是否有效。
+ * 有效字符串需满足：
+ * 1. 左括号必须用相同类型的右括号闭合。
+ * 2. 左括号必须以正确的顺序闭合。
+ * 3. 每个右括号都有一个对应的相同类型的左括号。
+ */
 public class LeetCode_020_isValid {
     public boolean isValid(String s) {
-        return false;
+        if (s == null || "".equalsIgnoreCase(s)) {
+            return true;
+        }
+        Deque<Character> stack = new LinkedList<>();
+
+        char[] elementArr = s.toCharArray();
+        for (char element : elementArr) {
+            if ('(' == element
+                    || '{' == element
+                    || '[' == element) {
+                stack.push(element);
+                continue;
+            }
+
+            char currentChar = stack.pop();
+            if (')' == element) {
+                if (currentChar != '(') {
+                    return false;
+                }
+            } else if ('}' == element) {
+                if (currentChar != '{') {
+                    return false;
+                }
+            } else if (']' == element) {
+                if (currentChar != '[') {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/test/java/leetcode/LeetCode_020_isValidTest.java
+++ src/test/java/leetcode/LeetCode_020_isValidTest.java
@@ -3,6 +3,7 @@ package leetcode;
 import org.github.fourth.leetcode.LeetCode_020_isValid;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -14,7 +15,7 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 public class LeetCode_020_isValidTest {
     private final LeetCode_020_isValid isValid = new LeetCode_020_isValid();
 
-    //    @ParameterizedTest
+    @ParameterizedTest
     @MethodSource("generateRightCase")
     public void test01(String s, boolean expectedResult) {
         Assertions.assertEquals(expectedResult, isValid.isValid(s));
