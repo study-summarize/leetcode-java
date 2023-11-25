04acd31a7f9bd9be99b8c890feb6a283ac31a05d
wangjie <wangjie_fourth@163.com>
2023-11-25 13:59:36 +0800

    add(code): 题目

--- src/main/java/org/github/fourth/leetcode/common/TreeNode.java
+++ src/main/java/org/github/fourth/leetcode/common/TreeNode.java
@@ -4,7 +4,7 @@ import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
 import java.util.List;
 
-@SuppressFBWarnings({"URF_UNREAD_FIELD"})
+@SuppressFBWarnings({"URF_UNREAD_FIELD", "URF_UNREAD_PUBLIC_OR_PROTECTED_FIELD"})
 public class TreeNode {
     public int val;
     public TreeNode left;
