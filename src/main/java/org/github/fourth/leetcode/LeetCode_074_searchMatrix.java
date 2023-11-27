f98d48106e7bcf64fdaa4dfc36407c2f813b1bb4
wangjie <jwang55@trip.com>
2023-11-27 13:33:38 +0800

    add: 搜索二维矩阵
    1. 二分搜索模板

--- src/main/java/org/github/fourth/leetcode/LeetCode_074_searchMatrix.java
+++ src/main/java/org/github/fourth/leetcode/LeetCode_074_searchMatrix.java
@@ -7,11 +7,37 @@ package org.github.fourth.leetcode;
  * 给你一个整数 target ，如果 target 在矩阵中，返回 true ；否则，返回 false 。
  */
 public class LeetCode_074_searchMatrix {
+
     /**
-     * 时间复杂度：
-     * 空间复杂度：
+     * 时间复杂度：O(logn)
+     * 空间复杂度：O(1)
      */
     public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0
+                || matrix[0] == null || matrix[0].length == 0) {
+            return false;
+        }
+        int start = 0;
+        int end = matrix.length * matrix[0].length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            int x = mid / matrix[0].length;
+            int y = mid % matrix[0].length;
+            if (matrix[x][y] > target) {
+                end = mid;
+            } else if (matrix[x][y] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+
+        if (matrix[start / matrix[0].length][start % matrix[0].length] == target) {
+            return true;
+        }
+        if (matrix[end / matrix[0].length][end % matrix[0].length] == target) {
+            return true;
+        }
         return false;
     }
 }
--- src/test/java/org/github/fourth/leetcode/LeetCode_074_searchMatrixTest.java
+++ src/test/java/org/github/fourth/leetcode/LeetCode_074_searchMatrixTest.java
@@ -2,6 +2,7 @@ package org.github.fourth.leetcode;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -14,8 +15,7 @@ public class LeetCode_074_searchMatrixTest {
 
     private final LeetCode_074_searchMatrix leetCode074SearchMatrix = new LeetCode_074_searchMatrix();
 
-
-    //    @ParameterizedTest
+    @ParameterizedTest
     @MethodSource("generateRightCase")
     public void test(int[][] matrix, int target, boolean expectedResult) {
         boolean actResult = leetCode074SearchMatrix.searchMatrix(matrix, target);
