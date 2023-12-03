f44f3bbd23001b2c682e7ce54c2c69e4ea48e575
wangjie <wangjie_fourth@163.com>
2023-12-04 01:34:45 +0800

    add: 链表的中间结点
    1. 快慢指针
    2. dummyNode技巧

--- src/main/java/org/github/fourth/leetcode/LeetCode_876_middleNode.java
+++ src/main/java/org/github/fourth/leetcode/LeetCode_876_middleNode.java
@@ -7,12 +7,58 @@ import org.github.fourth.leetcode.common.ListNode;
  * 如果有两个中间结点，则返回第二个中间结点。
  */
 public class LeetCode_876_middleNode {
+
     /**
-     * 时间复杂度：
-     * 空间复杂度：
+     * 快慢指针
+     *
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
+     * note：dummyNode
      */
-    public ListNode middleNode(ListNode head) {
-        return null;
+    public ListNode middleNode02(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode dummyNode = new ListNode();
+        dummyNode.next = head;
+
+        // 快慢指针
+        ListNode first = dummyNode;
+        ListNode second = dummyNode;
+        while (first.next != null && first.next.next != null) {
+            first = first.next.next;
+            second = second.next;
+        }
+        // 无论链表长度是奇数还是偶数；慢指针都需要进一步
+        return second.next;
+    }
+
+    /**
+     * 暴力解法：先求长度，再求中间结点位置
+     *
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
+     * note：dummyNode
+     */
+    public ListNode middleNode01(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        // 求长度
+        int length = 0;
+        ListNode node = head;
+        while (node != null) {
+            ++length;
+            node = node.next;
+        }
+
+        int pos = length / 2;
+        node = head;
+        while (pos != 0) {
+            --pos;
+            node = node.next;
+        }
+        return node;
     }
 
 
--- src/test/java/org/github/fourth/leetcode/LeetCode_876_middleNodeTest.java
+++ src/test/java/org/github/fourth/leetcode/LeetCode_876_middleNodeTest.java
@@ -1,10 +1,13 @@
 package org.github.fourth.leetcode;
 
 import org.github.fourth.leetcode.common.ListNode;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 import static org.github.fourth.leetcode.common.ListNode.build;
@@ -15,11 +18,24 @@ class LeetCode_876_middleNodeTest {
 
     private final LeetCode_876_middleNode leetCode876MiddleNode = new LeetCode_876_middleNode();
 
-//    @ParameterizedTest
+    @DisplayName("暴力解法")
+    @ParameterizedTest
     @MethodSource("generateRightCase")
     public void test01(ListNode head, ListNode expectedResult) {
-        ListNode result = leetCode876MiddleNode.middleNode(head);
+        commonTest(expectedResult, x -> x.middleNode01(head));
+    }
+
+    @DisplayName("快慢指针")
+    @ParameterizedTest
+    @MethodSource("generateRightCase")
+    public void test02(ListNode head, ListNode expectedResult) {
+        commonTest(expectedResult, x -> x.middleNode02(head));
+    }
+
+    private void commonTest(ListNode expectedResult, Function<LeetCode_876_middleNode, ListNode> function) {
+        ListNode result = function.apply(leetCode876MiddleNode);
         // todo: 如何验证？？？
+        Assertions.assertEquals(expectedResult.val, result.val);
     }
 
     public static Stream<Arguments> generateRightCase() {
@@ -28,7 +44,6 @@ class LeetCode_876_middleNodeTest {
                 arguments(build(new int[]{1, 2, 3, 4, 5, 6}), build(new int[]{4, 5, 6}))
         );
     }
-
 
 
 }
