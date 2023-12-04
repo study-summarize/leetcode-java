e23beb7a2de4e359dd6751c55c0db2474b802158
wangjie <jwang55@trip.com>
2023-12-04 20:59:02 +0800

    add: 得到链表的倒数第N个节点

--- src/main/java/org/github/fourth/leetcode/lcr/LCR_140_trainingPlan.java
+++ src/main/java/org/github/fourth/leetcode/lcr/LCR_140_trainingPlan.java
@@ -8,10 +8,35 @@ import org.github.fourth.leetcode.common.ListNode;
  */
 public class LCR_140_trainingPlan {
     /**
-     * 时间复杂度：
-     * 空间复杂度：
+     * 暴力方法：先求链表长度，找到倒数第 cnt 个节点
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
      */
     public ListNode trainingPlan(ListNode head, int cnt) {
-        return null;
+        if (head == null || cnt <= 0) {
+            return null;
+        }
+
+        ListNode dummyNode = new ListNode();
+        dummyNode.next = head;
+        // 先求长度
+        int length = 0;
+        while (dummyNode.next != null) {
+            length++;
+            dummyNode = dummyNode.next;
+        }
+        if (cnt > length) {
+            return null;
+        }
+
+        // 求节点的位置
+        int pos = length + 1 - cnt;
+        dummyNode = new ListNode();
+        dummyNode.next = head;
+        while (pos != 0) {
+            dummyNode = dummyNode.next;
+            --pos;
+        }
+        return dummyNode;
     }
 }
--- src/test/java/org/github/fourth/leetcode/lcr/LCR_140_trainingPlanTest.java
+++ src/test/java/org/github/fourth/leetcode/lcr/LCR_140_trainingPlanTest.java
@@ -1,7 +1,10 @@
 package org.github.fourth.leetcode.lcr;
 
+import org.github.fourth.ListNodeVerifyUtil;
 import org.github.fourth.leetcode.common.ListNode;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -14,16 +17,18 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 class LCR_140_trainingPlanTest {
     private final LCR_140_trainingPlan lcr140TrainingPlan = new LCR_140_trainingPlan();
 
-    //    @ParameterizedTest
+    @DisplayName("暴力方法")
+    @ParameterizedTest
     @MethodSource("generateRightCase")
-    public void test01(ListNode head, int cnt, int expectedResult) {
+    public void test01(ListNode head, int cnt, ListNode expectedResult) {
         ListNode actResult = lcr140TrainingPlan.trainingPlan(head, cnt);
-        // todo: 如何验证？？？
+        Assertions.assertTrue(ListNodeVerifyUtil.equals(actResult, expectedResult));
     }
+
 
     public static Stream<Arguments> generateRightCase() {
         return Stream.of(
-                arguments(build(new int[]{2, 4, 7, 8}), 1, 8)
+                arguments(build(new int[]{2, 4, 7, 8}), 1, build(8))
         );
     }
 }
