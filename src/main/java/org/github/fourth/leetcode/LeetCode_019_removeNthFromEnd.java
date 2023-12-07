766981a89025a6ea7d17e7899de9edd4d9c74d1a
wangjie <jwang55@trip.com>
2023-12-07 19:53:55 +0800

    add: 删除链表的倒数第 N 个结点
    1. dummyNode技巧
    2. 先后指针

--- src/main/java/org/github/fourth/leetcode/LeetCode_019_removeNthFromEnd.java
+++ src/main/java/org/github/fourth/leetcode/LeetCode_019_removeNthFromEnd.java
@@ -7,10 +7,38 @@ import org.github.fourth.leetcode.common.ListNode;
  */
 public class LeetCode_019_removeNthFromEnd {
     /**
-     * 时间复杂度：
-     * 空间复杂度：
+     * 前后指针
+     *
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
      */
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        return null;
+        if (head == null || n <= 0) {
+            return head;
+        }
+
+        ListNode dummyNode = new ListNode();
+        dummyNode.next = head;
+        // 找到倒数第n + 1个
+        ListNode fast = dummyNode;
+        ListNode last = dummyNode;
+        int moveCount = 0;
+        while (fast.next != null) {
+            if (++moveCount >= (n + 1)) {
+                // 找到第n + 1个节点
+                last = last.next;
+            }
+            fast = fast.next;
+        }
+
+        // 如果没找到n+1个节点
+        if (last == dummyNode) {
+            return null;
+        }
+
+        if (last.next != null) {
+            last.next = last.next.next;
+        }
+        return dummyNode.next;
     }
 }
--- src/test/java/org/github/fourth/leetcode/LeetCode_019_removeNthFromEndTest.java
+++ src/test/java/org/github/fourth/leetcode/LeetCode_019_removeNthFromEndTest.java
@@ -4,6 +4,7 @@ import org.github.fourth.ListNodeVerifyUtil;
 import org.github.fourth.leetcode.common.ListNode;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -17,7 +18,7 @@ class LeetCode_019_removeNthFromEndTest {
     private final LeetCode_019_removeNthFromEnd leetCode019RemoveNthFromEnd = new LeetCode_019_removeNthFromEnd();
 
 
-    //    @ParameterizedTest
+    @ParameterizedTest
     @MethodSource("generateRightCase")
     public void test01(ListNode head, int n, ListNode expectedResult) {
         ListNode actResult = leetCode019RemoveNthFromEnd.removeNthFromEnd(head, n);
@@ -27,6 +28,8 @@ class LeetCode_019_removeNthFromEndTest {
     public static Stream<Arguments> generateRightCase() {
         return Stream.of(
                 arguments(build(1, 2, 3, 4, 5), 2, build(1, 2, 3, 5)),
+                arguments(build(1, 2, 3, 4, 5), 1, build(1, 2, 3, 4)),
+                arguments(build(1, 2, 3, 4, 5), 3, build(1, 2, 4, 5)),
                 arguments(build(1), 1, build()),
                 arguments(build(1, 2), 1, build(1))
         );
