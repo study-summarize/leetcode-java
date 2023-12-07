9137bd6ab26202d6e07c28eca992809748a987b9
wangjie <jwang55@trip.com>
2023-12-07 18:44:45 +0800

    add: 两数相加
    1. dummyNode技巧

--- src/main/java/org/github/fourth/leetcode/LeetCode_002_addTwoNumbers.java
+++ src/main/java/org/github/fourth/leetcode/LeetCode_002_addTwoNumbers.java
@@ -8,11 +8,63 @@ import org.github.fourth.leetcode.common.ListNode;
  * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
  */
 public class LeetCode_002_addTwoNumbers {
+
     /**
-     * 时间复杂度：
-     * 空间复杂度：
+     * 想清楚各种情况就行，感觉没啥考察点
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
      */
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return null;
+        if (l1 == null && l2 == null) {
+            return null;
+        }
+        if (l1 == null || l2 == null) {
+            return l1 == null ? l2 : l1;
+        }
+
+        ListNode dummyNode = new ListNode();
+        ListNode cur = dummyNode;
+        boolean needAddOne = false;
+        while (l1 != null && l2 != null) {
+            int sum = l1.val + l2.val;
+            if (needAddOne) {
+                sum = sum + 1;
+            }
+            needAddOne = sum >= 10;
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        // l1还有剩余的
+        while (l1 != null) {
+            int sum = l1.val;
+            if (needAddOne) {
+                sum = sum + 1;
+            }
+            needAddOne = sum >= 10;
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
+            l1 = l1.next;
+        }
+
+        // l2还有剩余的
+        while (l2 != null) {
+            int sum = l2.val;
+            if (needAddOne) {
+                sum = sum + 1;
+            }
+            needAddOne = sum >= 10;
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
+            l2 = l2.next;
+        }
+
+        // 可能还会再进一
+        if (needAddOne) {
+            cur.next = new ListNode(1);
+        }
+        return dummyNode.next;
     }
 }
--- src/test/java/org/github/fourth/leetcode/LeetCode_002_addTwoNumbersTest.java
+++ src/test/java/org/github/fourth/leetcode/LeetCode_002_addTwoNumbersTest.java
@@ -4,6 +4,7 @@ import org.github.fourth.ListNodeVerifyUtil;
 import org.github.fourth.leetcode.common.ListNode;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -16,7 +17,7 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 class LeetCode_002_addTwoNumbersTest {
     private final LeetCode_002_addTwoNumbers leetCode002AddTwoNumbers = new LeetCode_002_addTwoNumbers();
 
-    //    @ParameterizedTest
+    @ParameterizedTest
     @MethodSource("generateRightCase")
     public void test01(ListNode l1, ListNode l2, ListNode expectedResult) {
         ListNode actResult = leetCode002AddTwoNumbers.addTwoNumbers(l1, l2);
@@ -25,9 +26,9 @@ class LeetCode_002_addTwoNumbersTest {
 
     public static Stream<Arguments> generateRightCase() {
         return Stream.of(
-                arguments(build(new int[]{2, 4, 3}), build(new int[]{5, 6, 4}), build(new int[]{7, 0, 8})),
-                arguments(build(new int[]{0}), build(new int[]{0}), build(new int[]{0})),
-                arguments(build(new int[]{9, 9, 9, 9, 9, 9, 9}), build(new int[]{9, 9, 9, 9}), build(new int[]{8, 9, 9, 9, 0, 0, 0, 1}))
+                arguments(build(2, 4, 3), build(5, 6, 4), build(7, 0, 8)),
+                arguments(build(0), build(0), build(0)),
+                arguments(build(9, 9, 9, 9, 9, 9, 9), build(9, 9, 9, 9), build(8, 9, 9, 9, 0, 0, 0, 1))
         );
     }
 }
