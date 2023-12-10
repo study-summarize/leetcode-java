28fe5685a66fecdc494d89d10901d879ac5e3d14
wangjie <wangjie_fourth@163.com>
2023-12-10 16:18:39 +0800

    add: 回文链表
    1. 快慢指针

--- src/main/java/org/github/fourth/leetcode/LeetCode_234_isPalindrome.java
+++ src/main/java/org/github/fourth/leetcode/LeetCode_234_isPalindrome.java
@@ -7,10 +7,50 @@ import org.github.fourth.leetcode.common.ListNode;
  */
 public class LeetCode_234_isPalindrome {
     /**
-     * 时间复杂度：
-     * 空间复杂度：
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
      */
     public boolean isPalindrome(ListNode head) {
-        return false;
+        if (head == null) {
+            return true;
+        }
+        // 求中点
+        ListNode middleNode = getMiddleOfListNode(head);
+        // 将后半部分链表翻转
+        ListNode tail = reverseListNode(middleNode.next);
+        middleNode.next = null;
+        // 判断回文
+        while (head != null && tail != null) {
+            int num1 = head.val;
+            int num2 = tail.val;
+            if (num1 != num2) {
+                return false;
+            }
+            head = head.next;
+            tail = tail.next;
+        }
+        return true;
+    }
+
+    public ListNode reverseListNode(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
+        }
+        return prev;
+    }
+
+    public ListNode getMiddleOfListNode(ListNode head) {
+        ListNode fast = head.next;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
     }
 }
--- src/test/java/org/github/fourth/leetcode/LeetCode_234_isPalindromeTest.java
+++ src/test/java/org/github/fourth/leetcode/LeetCode_234_isPalindromeTest.java
@@ -3,6 +3,7 @@ package org.github.fourth.leetcode;
 import org.github.fourth.leetcode.common.ListNode;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -17,7 +18,7 @@ class LeetCode_234_isPalindromeTest {
     private final LeetCode_234_isPalindrome leetCode234IsPalindrome = new LeetCode_234_isPalindrome();
 
 
-    //    @ParameterizedTest
+    @ParameterizedTest
     @MethodSource("generateRightCase")
     public void test01(ListNode head, boolean expectedResult) {
         boolean actResult = leetCode234IsPalindrome.isPalindrome(head);
