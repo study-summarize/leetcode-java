b45bad30ee777e14d4b6add1b810e7efcb17c232
wangjie <wangjie_fourth@163.com>
2023-12-10 16:13:23 +0800

    add: 两数相加 II
    1. 头插法
    2. dummyNode技巧

--- src/main/java/org/github/fourth/leetcode/LeetCode_445_addTwoNumbers.java
+++ src/main/java/org/github/fourth/leetcode/LeetCode_445_addTwoNumbers.java
@@ -2,16 +2,113 @@ package org.github.fourth.leetcode;
 
 import org.github.fourth.leetcode.common.ListNode;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * 给你两个 非空 链表来代表两个非负整数。数字最高位位于链表开始位置。它们的每个节点只存储一位数字。将这两数相加会返回一个新的链表。
  * 你可以假设除了数字 0 之外，这两个数字都不会以零开头。
  */
 public class LeetCode_445_addTwoNumbers {
     /**
-     * 时间复杂度：
-     * 空间复杂度：
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
      */
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return null;
+    public ListNode addTwoNumbers01(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) {
+            return null;
+        }
+        ListNode dummyNode = new ListNode();
+        Deque<Integer> stack1 = new ArrayDeque<>();
+        Deque<Integer> stack2 = new ArrayDeque<>();
+
+        while (l1 != null) {
+            stack1.push(l1.val);
+            l1 = l1.next;
+        }
+        while (l2 != null) {
+            stack2.push(l2.val);
+            l2 = l2.next;
+        }
+
+        int carry = 0;
+        while (!stack1.isEmpty() && !stack2.isEmpty()) {
+            int num1 = stack1.pop();
+            int num2 = stack2.pop();
+            int sum = num1 + num2 + carry;
+            int value = sum % 10;
+            carry = sum / 10;
+
+            ListNode newNode = new ListNode(value);
+            ListNode temp = dummyNode.next;
+            dummyNode.next = newNode;
+            newNode.next = temp;
+        }
+
+        while (!stack1.isEmpty()) {
+            int sum = stack1.pop() + carry;
+            int value = sum % 10;
+            carry = sum / 10;
+
+            ListNode newNode = new ListNode(value);
+            ListNode temp = dummyNode.next;
+            dummyNode.next = newNode;
+            newNode.next = temp;
+        }
+
+        while (!stack2.isEmpty()) {
+            int sum = stack2.pop() + carry;
+            int value = sum % 10;
+            carry = sum / 10;
+
+            ListNode newNode = new ListNode(value);
+            ListNode temp = dummyNode.next;
+            dummyNode.next = newNode;
+            newNode.next = temp;
+        }
+
+        if (carry != 0) {
+            ListNode newNode = new ListNode(carry);
+            ListNode temp = dummyNode.next;
+            dummyNode.next = newNode;
+            newNode.next = temp;
+        }
+        return dummyNode.next;
+    }
+
+    /**
+     * 对上面的代码简化一下
+     */
+    public ListNode addTwoNumbers02(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) {
+            return null;
+        }
+        ListNode dummyNode = new ListNode();
+        Deque<Integer> stack1 = new ArrayDeque<>();
+        Deque<Integer> stack2 = new ArrayDeque<>();
+
+        while (l1 != null) {
+            stack1.push(l1.val);
+            l1 = l1.next;
+        }
+        while (l2 != null) {
+            stack2.push(l2.val);
+            l2 = l2.next;
+        }
+
+        int carry = 0;
+        while (!stack1.isEmpty() || !stack2.isEmpty() || carry != 0) {
+            int num1 = stack1.isEmpty() ? 0 : stack1.pop();
+            int num2 = stack2.isEmpty() ? 0 : stack2.pop();
+            int sum = num1 + num2 + carry;
+            int value = sum % 10;
+            carry = sum / 10;
+
+            ListNode newNode = new ListNode(value);
+            ListNode temp = dummyNode.next;
+            dummyNode.next = newNode;
+            newNode.next = temp;
+        }
+        return dummyNode.next;
     }
 }
--- src/test/java/org/github/fourth/leetcode/LeetCode_445_addTwoNumbersTest.java
+++ src/test/java/org/github/fourth/leetcode/LeetCode_445_addTwoNumbersTest.java
@@ -4,6 +4,7 @@ import org.github.fourth.ListNodeVerifyUtil;
 import org.github.fourth.leetcode.common.ListNode;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -17,10 +18,18 @@ class LeetCode_445_addTwoNumbersTest {
     private final LeetCode_445_addTwoNumbers leetCode445AddTwoNumbers = new LeetCode_445_addTwoNumbers();
 
 
-    //    @ParameterizedTest
+    @ParameterizedTest
     @MethodSource("generateRightCase")
     public void test01(ListNode l1, ListNode l2, ListNode expectedResult) {
-        ListNode actResult = leetCode445AddTwoNumbers.addTwoNumbers(l1, l2);
+        ListNode actResult = leetCode445AddTwoNumbers.addTwoNumbers01(l1, l2);
+        Assertions.assertTrue(ListNodeVerifyUtil.equals(expectedResult, actResult));
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("generateRightCase")
+    public void test02(ListNode l1, ListNode l2, ListNode expectedResult) {
+        ListNode actResult = leetCode445AddTwoNumbers.addTwoNumbers02(l1, l2);
         Assertions.assertTrue(ListNodeVerifyUtil.equals(expectedResult, actResult));
     }
 
