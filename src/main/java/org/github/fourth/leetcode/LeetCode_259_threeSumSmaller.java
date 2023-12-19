dcd1a7b294eae8ffb53e2dbe90dce51fcd363a77
wangjie <jwang55@trip.com>
2023-12-19 14:07:43 +0800

    add: 较小的三数之和

--- src/main/java/org/github/fourth/leetcode/LeetCode_259_threeSumSmaller.java
+++ src/main/java/org/github/fourth/leetcode/LeetCode_259_threeSumSmaller.java
@@ -7,10 +7,34 @@ import java.util.Arrays;
  */
 public class LeetCode_259_threeSumSmaller {
     /**
-     * 时间复杂度：
-     * 空间复杂度：
+     * 时间复杂度：O(n)???
+     * 空间复杂度：O(1)
      */
     public int threeSumSmaller(int[] nums, int target) {
+        int count = 0;
+        if (nums == null || nums.length == 0) {
+            return count;
+        }
+
+        Arrays.sort(nums);
+        for (int i = nums.length - 1; i > 0; i--) {
+            int start = 0;
+            int end = i - 1;
+            while (start < end) {
+                if (nums[i] + nums[start] + nums[end] < target) {
+                    count = count + (end - start);
+                    start++;
+                } else {
+                    // 这个其实是跟题目的i,j,k要求有关
+                    end--;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public int threeSumSmallerExample(int[] nums, int target) {
         int count = 0;
         if (nums == null || nums.length == 0) {
             return count;
--- src/test/java/org/github/fourth/leetcode/LeetCode_259_threeSumSmallerTest.java
+++ src/test/java/org/github/fourth/leetcode/LeetCode_259_threeSumSmallerTest.java
@@ -2,6 +2,7 @@ package org.github.fourth.leetcode;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -13,10 +14,17 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 class LeetCode_259_threeSumSmallerTest {
     private final LeetCode_259_threeSumSmaller threeSumSmaller = new LeetCode_259_threeSumSmaller();
 
-    //    @ParameterizedTest
+    @ParameterizedTest
+    @MethodSource("generateRightCase")
+    public void test01(int[] nums, int target, int expectedResult) {
+        int actResult = threeSumSmaller.threeSumSmaller(nums, target);
+        Assertions.assertEquals(expectedResult, actResult);
+    }
+
+    @ParameterizedTest
     @MethodSource("generateRightCase")
     public void test(int[] nums, int target, int expectedResult) {
-        int actResult = threeSumSmaller.threeSumSmaller(nums, target);
+        int actResult = threeSumSmaller.threeSumSmallerExample(nums, target);
         Assertions.assertEquals(expectedResult, actResult);
     }
 
