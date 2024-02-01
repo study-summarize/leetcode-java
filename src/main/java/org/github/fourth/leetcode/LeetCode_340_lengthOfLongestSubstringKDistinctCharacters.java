9d37b63dd68d7e9d62c015751b5064c223107b76
wangjie <jwang55@trip.com>
2024-02-01 16:19:10 +0800

    add: 至多包含 K 个不同字符的最长子串
    1. 同向型-滑动窗口双指针

--- src/main/java/org/github/fourth/leetcode/LeetCode_340_lengthOfLongestSubstringKDistinctCharacters.java
+++ src/main/java/org/github/fourth/leetcode/LeetCode_340_lengthOfLongestSubstringKDistinctCharacters.java
@@ -7,13 +7,47 @@ import java.util.Map;
  * 给你一个字符串 s 和一个整数 k ，请你找出 至多 包含 k 个 不同 字符的最长子串，并返回该子串的长度。
  */
 public class LeetCode_340_lengthOfLongestSubstringKDistinctCharacters {
+
     /**
+     * 同向型双指针-滑动窗口
      * 时间复杂度：
      * 空间复杂度：
      */
     public int lengthOfLongestSubstringKDistinctCharacters(String s, int k) {
+        if (s == null || s.isEmpty() || k < 0) {
+            return 0;
+        }
+        int result = Integer.MIN_VALUE;
+        int start = 0;
+        int end = 0;
+
+        Map<Character, Integer> containCharMap = new HashMap<>();
+        for (; start < s.length(); start++) {
+            while (end < s.length()) {
+                // 不符合条件，向右滑动
+                if (containCharMap.containsKey(s.charAt(end))
+                        || containCharMap.keySet().size() < k) {
+                    containCharMap.put(s.charAt(end), containCharMap.getOrDefault(s.charAt(end), 0) + 1);
+                    end++;
+                } else {
+                    break;
+                }
+            }
+            // 开始收缩窗口
+            result = Math.max(result, end - start);
+            Integer size = containCharMap.getOrDefault(s.charAt(start), 0);
+            if (size == 1) {
+                containCharMap.remove(s.charAt(start));
+            } else {
+                containCharMap.put(s.charAt(start), size - 1);
+            }
+        }
+        return result;
+    }
+
+    public int lengthOfLongestSubstringKDistinctCharactersExample(String s, int k) {
         int result = 0;
-        if (s == null || s.length() == 0 || k == 0) {
+        if (s == null || s.isEmpty() || k == 0) {
             return result;
         }
 
--- src/test/java/org/github/fourth/leetcode/LeetCode_340_lengthOfLongestSubstringKDistinctCharactersTest.java
+++ src/test/java/org/github/fourth/leetcode/LeetCode_340_lengthOfLongestSubstringKDistinctCharactersTest.java
@@ -2,6 +2,7 @@ package org.github.fourth.leetcode;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -13,10 +14,18 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 class LeetCode_340_lengthOfLongestSubstringKDistinctCharactersTest {
     private final LeetCode_340_lengthOfLongestSubstringKDistinctCharacters lengthOfLongestSubstringKDistinctCharacters = new LeetCode_340_lengthOfLongestSubstringKDistinctCharacters();
 
-    //    @ParameterizedTest
+
+    @ParameterizedTest
     @MethodSource("generateRightCase")
-    public void test(String s, int k, int expectedResult) {
+    public void test01(String s, int k, int expectedResult) {
         int actResult = lengthOfLongestSubstringKDistinctCharacters.lengthOfLongestSubstringKDistinctCharacters(s, k);
+        Assertions.assertEquals(expectedResult, actResult);
+    }
+
+    @ParameterizedTest
+    @MethodSource("generateRightCase")
+    public void testExample(String s, int k, int expectedResult) {
+        int actResult = lengthOfLongestSubstringKDistinctCharacters.lengthOfLongestSubstringKDistinctCharactersExample(s, k);
         Assertions.assertEquals(expectedResult, actResult);
     }
 
