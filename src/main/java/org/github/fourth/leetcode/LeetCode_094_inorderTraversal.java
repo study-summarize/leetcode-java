70c002e30ca33ab3c670968348eb6b3f9017322e
wangjie <jwang55@trip.com>
2024-02-06 13:28:19 +0800

    add: 二叉树的中序遍历
    1. 递归法
    2. 非递归：寻找最左子树

--- src/main/java/org/github/fourth/leetcode/LeetCode_094_inorderTraversal.java
+++ src/main/java/org/github/fourth/leetcode/LeetCode_094_inorderTraversal.java
@@ -2,17 +2,64 @@ package org.github.fourth.leetcode;
 
 import org.github.fourth.leetcode.common.TreeNode;
 
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
+ * 二叉树的中序遍历：左子树 ----> 根节点 ----> 右子树
  * 给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
  */
 public class LeetCode_094_inorderTraversal {
     /**
+     * 递归法：
      * 时间复杂度：
      * 空间复杂度：
      */
-    public List<Integer> inorderTraversal(TreeNode root) {
-        return null;
+    public List<Integer> inorderTraversal01(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        inorderTraversal01Helper(result, root);
+        return result;
+    }
+
+    private void inorderTraversal01Helper(List<Integer> result, TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inorderTraversal01Helper(result, root.left);
+        result.add(root.val);
+        inorderTraversal01Helper(result, root.right);
+    }
+
+    /**
+     * 非递归：用栈模拟递归
+     * 时间复杂度：
+     * 空间复杂度：
+     */
+    public List<Integer> inorderTraversal02(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Deque<TreeNode> stack = new LinkedList<>();
+        while (!stack.isEmpty() || root != null) {
+            // 先找到当前根节点的最左子树
+            while (root != null) {
+                // 先把根节点放进来，等左子树遍历完，就开始遍历这个
+                stack.push(root);
+                root = root.left;
+            }
+            // 此时已经到root跟节点的最左底端
+            TreeNode currentNode = stack.pop();
+            // currentNode其实就变成根节点了
+            result.add(currentNode.val);
+            // 开始右节点遍历
+            root = currentNode.right;
+        }
+        return result;
     }
 }
--- src/test/java/org/github/fourth/leetcode/LeetCode_094_inorderTraversalTest.java
+++ src/test/java/org/github/fourth/leetcode/LeetCode_094_inorderTraversalTest.java
@@ -1,7 +1,9 @@
 package org.github.fourth.leetcode;
 
 import org.github.fourth.leetcode.common.TreeNode;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -15,15 +17,23 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 class LeetCode_094_inorderTraversalTest {
     private final LeetCode_094_inorderTraversal leetCode094InorderTraversal = new LeetCode_094_inorderTraversal();
 
-    //    @ParameterizedTest
+    @ParameterizedTest
     @MethodSource("generateRightCase")
     public void test01(TreeNode root, List<Integer> expectedResult) {
-        List<Integer> actResult = leetCode094InorderTraversal.inorderTraversal(root);
+        List<Integer> actResult = leetCode094InorderTraversal.inorderTraversal01(root);
+        Assertions.assertArrayEquals(expectedResult.toArray(), actResult.toArray());
+    }
+
+    @ParameterizedTest
+    @MethodSource("generateRightCase")
+    public void test02(TreeNode root, List<Integer> expectedResult) {
+        List<Integer> actResult = leetCode094InorderTraversal.inorderTraversal02(root);
+        Assertions.assertArrayEquals(expectedResult.toArray(), actResult.toArray());
     }
 
     public static Stream<Arguments> generateRightCase() {
         return Stream.of(
-                arguments(TreeNode.build(Arrays.asList(1, null, 2, 3)), Arrays.asList(1, 3, 2)),
+                arguments(TreeNode.build(Arrays.asList(1, null, 2, null, null, 3)), Arrays.asList(1, 3, 2)),
                 arguments(TreeNode.build(List.of()), List.of()),
                 arguments(TreeNode.build(List.of(1)), List.of(1))
         );
