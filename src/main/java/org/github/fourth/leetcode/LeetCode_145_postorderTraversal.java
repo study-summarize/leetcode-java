923bc2c352974f3e8aa9cd75b591fdca73861396
wangjie <jwang55@trip.com>
2024-02-06 13:46:56 +0800

    add: 二叉树的后序遍历
    1. 递归法
    2. 非递归：俩个栈
    3. 非递归：一个栈 + 标识

--- src/main/java/org/github/fourth/leetcode/LeetCode_145_postorderTraversal.java
+++ src/main/java/org/github/fourth/leetcode/LeetCode_145_postorderTraversal.java
@@ -2,17 +2,61 @@ package org.github.fourth.leetcode;
 
 import org.github.fourth.leetcode.common.TreeNode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * 二叉树的后序遍历：左子树 ----> 右子树 ----> 根节点
  * 给你一棵二叉树的根节点 root ，返回其节点值的 后序遍历 。
  */
 public class LeetCode_145_postorderTraversal {
     /**
+     * 递归法
      * 时间复杂度：
      * 空间复杂度：
      */
-    public List<Integer> postorderTraversal(TreeNode root) {
-        return null;
+    public List<Integer> postorderTraversal01(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        postorderTraversal01Helper(result, root);
+        return result;
     }
+
+    private void postorderTraversal01Helper(List<Integer> result, TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        postorderTraversal01Helper(result, root.left);
+        postorderTraversal01Helper(result, root.right);
+        result.add(root.val);
+    }
+
+    /**
+     * 非递归：利用俩个栈
+     * 时间复杂度：
+     * 空间复杂度：
+     */
+    public List<Integer> postorderTraversal02(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        return result;
+    }
+
+    /**
+     * 非递归：一个栈 + 标识
+     * 时间复杂度：
+     * 空间复杂度：
+     */
+    public List<Integer> postorderTraversal03(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        return result;
+    }
+
 }
--- src/test/java/org/github/fourth/leetcode/LeetCode_145_postorderTraversalTest.java
+++ src/test/java/org/github/fourth/leetcode/LeetCode_145_postorderTraversalTest.java
@@ -1,7 +1,9 @@
 package org.github.fourth.leetcode;
 
 import org.github.fourth.leetcode.common.TreeNode;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -9,22 +11,34 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 @DisplayName("二叉树的后序遍历")
 class LeetCode_145_postorderTraversalTest {
     private final LeetCode_145_postorderTraversal leetCode145PostorderTraversal = new LeetCode_145_postorderTraversal();
 
-    //    @ParameterizedTest
+    @ParameterizedTest
     @MethodSource("generateRightCase")
     public void test01(TreeNode root, List<Integer> expectedResult) {
-        List<Integer> actResult = leetCode145PostorderTraversal.postorderTraversal(root);
+        List<Integer> actResult = leetCode145PostorderTraversal.postorderTraversal01(root);
+        Assertions.assertArrayEquals(expectedResult.toArray(), actResult.toArray());
+    }
+//    @ParameterizedTest
+    @MethodSource("generateRightCase")
+    public void test02(TreeNode root, List<Integer> expectedResult) {
+        List<Integer> actResult = leetCode145PostorderTraversal.postorderTraversal02(root);
+        Assertions.assertArrayEquals(expectedResult.toArray(), actResult.toArray());
+    }
+//    @ParameterizedTest
+    @MethodSource("generateRightCase")
+    public void test03(TreeNode root, List<Integer> expectedResult) {
+        List<Integer> actResult = leetCode145PostorderTraversal.postorderTraversal03(root);
+        Assertions.assertArrayEquals(expectedResult.toArray(), actResult.toArray());
     }
 
     public static Stream<Arguments> generateRightCase() {
         return Stream.of(
-                arguments(TreeNode.build(Arrays.asList(1, null, 2, 3)), Arrays.asList(3, 2, 1)),
+                arguments(TreeNode.build(Arrays.asList(1, null, 2, null, null, 3)), Arrays.asList(3, 2, 1)),
                 arguments(TreeNode.build(List.of()), List.of()),
                 arguments(TreeNode.build(List.of(1)), List.of(1))
         );
