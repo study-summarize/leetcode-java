f236bf5845124321362eaa754c543af910190b2e
wangjie <jwang55@trip.com>
2024-02-07 12:22:23 +0800

    add: 从中序与后序遍历序列构造二叉树

--- src/main/java/org/github/fourth/leetcode/LeetCode_106_buildTree.java
+++ src/main/java/org/github/fourth/leetcode/LeetCode_106_buildTree.java
@@ -7,10 +7,52 @@ import org.github.fourth.leetcode.common.TreeNode;
  */
 public class LeetCode_106_buildTree {
     /**
+     * 思路：
+     * - 根据后序遍历确定根节点值
+     * - 再到中序，确定左右子树的位置
      * 时间复杂度：
      * 空间复杂度：
      */
-    public TreeNode buildTree(int[] inorder, int[] postorder) {
-        return null;
+    public TreeNode buildTreeExample(int[] inorder, int[] postorder) {
+        if (inorder == null || inorder.length == 0
+                || postorder == null || postorder.length == 0) {
+            return null;
+        }
+        return helper(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
+    }
+
+    private TreeNode helper(int[] inorder, int iStart, int iEnd, int[] postorder, int pStart, int pEnd) {
+        // 防止越界
+        if (iStart > iEnd || pStart > pEnd) {
+            return null;
+        }
+
+        // 1. 根据后序序列得到根节点值
+        TreeNode root = new TreeNode(postorder[pEnd]);
+
+        // 2. 找到中序根节点的位置
+        int iRootPos = findRootPos(inorder, root.val);
+        /*
+         * 2.a 遍历左子树
+         * 中序左子树[iStart, iRootPos - 1]
+         * 后序左子树[pStart, pStart + (iRootPos - 1 - iStart)]
+         */
+        root.left = helper(inorder, iStart, iRootPos - 1, postorder, pStart, pStart + iRootPos - 1 - iStart);
+        /*
+         * 2.b 遍历右子树
+         * 中序右子树[iRootPos + 1, iEnd]
+         * 后序右子树[pStart - iStart + iRootPos, pEnd - 1]
+         */
+        root.right = helper(inorder, iRootPos + 1, iEnd, postorder, pStart - iStart + iRootPos, pEnd - 1);
+        return root;
+    }
+
+    private int findRootPos(int[] inorder, int val) {
+        for (int i = 0; i < inorder.length; i++) {
+            if (inorder[i] == val) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
--- src/test/java/org/github/fourth/leetcode/LeetCode_106_buildTreeTest.java
+++ src/test/java/org/github/fourth/leetcode/LeetCode_106_buildTreeTest.java
@@ -1,10 +1,13 @@
 package org.github.fourth.leetcode;
 
 import org.github.fourth.leetcode.common.TreeNode;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -15,16 +18,16 @@ class LeetCode_106_buildTreeTest {
     private final LeetCode_106_buildTree leetCode106BuildTree = new LeetCode_106_buildTree();
 
 
-    //    @ParameterizedTest
+    @ParameterizedTest
     @MethodSource("generateRightCase")
-    public void test01(int[] inorder, int[] postorder, TreeNode expectedResult) {
-        TreeNode actResult = leetCode106BuildTree.buildTree(inorder, postorder);
-
+    public void testExample(int[] inorder, int[] postorder, TreeNode expectedResult) {
+        TreeNode actResult = leetCode106BuildTree.buildTreeExample(inorder, postorder);
+        Assertions.assertArrayEquals(expectedResult.preIntegerList().toArray(), actResult.preIntegerList().toArray());
     }
 
     public static Stream<Arguments> generateRightCase() {
         return Stream.of(
-                arguments(new int[]{9, 3, 15, 20, 7}, new int[]{9, 15, 7, 20, 3}, TreeNode.build(List.of(3, 9, 20, null, null, 15, 7))),
+                arguments(new int[]{9, 3, 15, 20, 7}, new int[]{9, 15, 7, 20, 3}, TreeNode.build(Arrays.asList(3, 9, 20, null, null, 15, 7))),
                 arguments(new int[]{-1}, new int[]{-1}, TreeNode.build(List.of(-1)))
         );
     }
