3e5a45c088f0b8437bee19b4410d49ea2be97b35
wangjie <jwang55@trip.com>
2024-02-28 16:20:44 +0800

    add: 全排列
    note:
    1. 排列问题 --> 回溯法
    2. 排列问题似乎pos就没啥用了

--- src/main/java/org/github/fourth/leetcode/LeetCode_046_permute.java
+++ src/main/java/org/github/fourth/leetcode/LeetCode_046_permute.java
@@ -1,16 +1,50 @@
 package org.github.fourth.leetcode;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
  */
 public class LeetCode_046_permute {
     /**
+     * 回溯法
      * 时间复杂度：
      * 空间复杂度：
      */
-    public List<List<Integer>> permute(int[] nums) {
-        return null;
+    public List<List<Integer>> permute01(int[] nums) {
+        // 解集
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null) {
+            return result;
+        }
+
+        // 单一解集
+        List<Integer> list = new ArrayList<>();
+
+        // 递归拆分问题
+        helper(result, list, nums);
+        return result;
+    }
+
+    private void helper(List<List<Integer>> result, List<Integer> list, int[] nums) {
+        // 1. 递归什么时候结束？pos 达到nums长度时;或者list的长度为3时
+        // 2. 单一解集什么时候加入解集中？list长度为3时
+        if (list.size() == nums.length) {
+            result.add(new ArrayList<>(list));
+            return;
+        }
+
+        // 3. 拆分问题
+        for (int i = 0; i < nums.length; i++) {
+            // 3.1 扩展单一解集
+            if (list.contains(nums[i])) {
+                continue;
+            }
+            list.add(nums[i]);
+            // 3.2 递归解集
+            helper(result, list, nums);
+            // 3.3 回溯法
+            list.remove(list.size() - 1);
+        }
     }
 }
--- src/test/java/org/github/fourth/leetcode/LeetCode_046_permuteTest.java
+++ src/test/java/org/github/fourth/leetcode/LeetCode_046_permuteTest.java
@@ -2,6 +2,7 @@ package org.github.fourth.leetcode;
 
 import org.github.fourth.VerifyUtils;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -14,16 +15,16 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 class LeetCode_046_permuteTest {
     private final LeetCode_046_permute leetCode046Permute = new LeetCode_046_permute();
 
-    //    @ParameterizedTest
+    @ParameterizedTest
     @MethodSource("generateRightCase")
     public void test01(int[] nums, List<List<Integer>> expectedResult) {
-        List<List<Integer>> actResult = leetCode046Permute.permute(nums);
+        List<List<Integer>> actResult = leetCode046Permute.permute01(nums);
         VerifyUtils.assertListInListEquals(expectedResult, actResult);
     }
 
     public static Stream<Arguments> generateRightCase() {
         return Stream.of(
-                arguments(new int[]{1, 2, 2}, List.of(List.of(1, 2, 3), List.of(1, 3, 2), List.of(2, 1, 3), List.of(2, 3, 1), List.of(3, 1, 2), List.of(3, 2, 1))),
+                arguments(new int[]{1, 2, 3}, List.of(List.of(1, 2, 3), List.of(1, 3, 2), List.of(2, 1, 3), List.of(2, 3, 1), List.of(3, 1, 2), List.of(3, 2, 1))),
                 arguments(new int[]{0, 1}, List.of(List.of(0, 1), List.of(1, 0))),
                 arguments(new int[]{1}, List.of(List.of(1)))
         );
