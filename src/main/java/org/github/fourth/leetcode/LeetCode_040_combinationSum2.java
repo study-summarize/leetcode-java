67f1cf57e4eb0c17633fda38723e78c7393a88e2
wangjie <jwang55@trip.com>
2024-02-28 18:39:44 +0800

    add: 组合总和 II
    note: 组合 --> 回溯法

--- src/main/java/org/github/fourth/leetcode/LeetCode_040_combinationSum2.java
+++ src/main/java/org/github/fourth/leetcode/LeetCode_040_combinationSum2.java
@@ -1,5 +1,7 @@
 package org.github.fourth.leetcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -9,10 +11,50 @@ import java.util.List;
  */
 public class LeetCode_040_combinationSum2 {
     /**
+     * 回溯法
      * 时间复杂度：
      * 空间复杂度：
      */
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        return null;
+    public List<List<Integer>> combinationSum201(int[] candidates, int target) {
+        // 解集
+        List<List<Integer>> result = new ArrayList<>();
+        if (candidates == null) {
+            return result;
+        }
+
+        Arrays.sort(candidates);
+        // 单一解集
+        List<Integer> list = new ArrayList<>();
+
+        // 递归解决问题
+        helper(result, list, candidates, target, 0);
+        return result;
+    }
+
+    private void helper(List<List<Integer>> result, List<Integer> list, int[] candidates, int target, int pos) {
+        // 1. 递归什么时候结束？target小于0
+        if (target < 0) {
+            return;
+        }
+        // 2. 单一解集什么时候加入解集？target为0
+        if (target == 0) {
+            result.add(new ArrayList<>(list));
+            return;
+        }
+
+        // 3. 拆分子问题
+        for (int i = pos; i < candidates.length; i++) {
+            // 横向的重复元素不需要参与计算
+            if (i != pos && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+            // 3.1 扩展子集
+            list.add(candidates[i]);
+            // 3.2 递归求解问题
+            helper(result, list, candidates, target - candidates[i], i + 1);
+            // 3.3 回溯
+            list.remove(list.size() - 1);
+        }
+
     }
 }
--- src/test/java/org/github/fourth/leetcode/LeetCode_040_combinationSum2Test.java
+++ src/test/java/org/github/fourth/leetcode/LeetCode_040_combinationSum2Test.java
@@ -1,8 +1,8 @@
 package org.github.fourth.leetcode;
 
 import org.github.fourth.VerifyUtils;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -15,16 +15,16 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 class LeetCode_040_combinationSum2Test {
     private final LeetCode_040_combinationSum2 leetCode040CombinationSum2 = new LeetCode_040_combinationSum2();
 
-    //    @ParameterizedTest
+    @ParameterizedTest
     @MethodSource("generateRightCase")
     public void test01(int[] candidates, int target, List<List<Integer>> expectedResult) {
-        List<List<Integer>> actResult = leetCode040CombinationSum2.combinationSum2(candidates, target);
+        List<List<Integer>> actResult = leetCode040CombinationSum2.combinationSum201(candidates, target);
         VerifyUtils.assertListInListEquals(expectedResult, actResult);
     }
 
     public static Stream<Arguments> generateRightCase() {
         return Stream.of(
-                arguments(new int[]{10, 1, 2, 7, 6, 1, 5}, 0, List.of(List.of(1, 1, 6), List.of(1, 2, 5), List.of(1, 7), List.of(2, 6))),
+                arguments(new int[]{10, 1, 2, 7, 6, 1, 5}, 8, List.of(List.of(1, 1, 6), List.of(1, 2, 5), List.of(1, 7), List.of(2, 6))),
                 arguments(new int[]{2, 5, 2, 1, 2}, 5, List.of(List.of(1, 2, 2), List.of(5)))
         );
     }
