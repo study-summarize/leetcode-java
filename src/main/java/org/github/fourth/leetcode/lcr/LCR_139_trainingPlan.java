10ccdee94d2408f0bcc87d77f3e2b292a53c5429
wangjie <jwang55@trip.com>
2024-02-29 15:20:36 +0800

    update

--- src/main/java/org/github/fourth/leetcode/lcr/LCR_139_trainingPlan.java
+++ src/main/java/org/github/fourth/leetcode/lcr/LCR_139_trainingPlan.java
@@ -4,11 +4,12 @@ package org.github.fourth.leetcode.lcr;
  * 教练使用整数数组 actions 记录一系列核心肌群训练项目编号。为增强训练趣味性，需要将所有奇数编号训练项目调整至偶数编号训练项目之前。请将调整后的训练项目编号以 数组 形式返回。
  */
 public class LCR_139_trainingPlan {
+
     /**
      * 时间复杂度：O(n)
      * 空间复杂度：O(1)
      */
-    public int[] trainingPlan(int[] actions) {
+    public int[] trainingPlanExample(int[] actions) {
         int i = 0, j = actions.length - 1, tmp;
         while (i < j) {
             while (i < j && (actions[i] & 1) == 1) i++;
--- src/test/java/org/github/fourth/leetcode/lcr/LCR_139_trainingPlanTest.java
+++ src/test/java/org/github/fourth/leetcode/lcr/LCR_139_trainingPlanTest.java
@@ -1,11 +1,13 @@
 package org.github.fourth.leetcode.lcr;
 
+import org.github.fourth.VerifyUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.params.provider.Arguments.arguments;
@@ -17,14 +19,17 @@ class LCR_139_trainingPlanTest {
 
     @ParameterizedTest
     @MethodSource("generateRightCase")
-    public void test(int[] actions, int[] expectedResult) {
-        int[] actResult = lcr139TrainingPlan.trainingPlan(actions);
-        Assertions.assertArrayEquals(expectedResult, actResult);
+    public void testExample(int[] actions, int[] expectedResult, int pos) {
+        int[] actResult = lcr139TrainingPlan.trainingPlanExample(actions);
+        VerifyUtils.assertListEquals(List.of(expectedResult), List.of(actResult));
+        for (int i = 0; i <= pos; i++) {
+            Assertions.assertEquals(1, actResult[i] % 2);
+        }
     }
 
     public static Stream<Arguments> generateRightCase() {
         return Stream.of(
-                arguments(new int[]{1, 2, 3, 4, 5}, new int[]{1, 5, 3, 4, 2})
+                arguments(new int[]{1, 2, 3, 4, 5}, new int[]{1, 5, 3, 4, 2}, 2)
         );
     }
 
